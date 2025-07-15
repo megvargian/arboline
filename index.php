f6e7ba310488c7f6db7f15117c343e2549c6eff9
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-07-15 16:04:40 +0300

    remove slick js

--- index.php
+++ index.php
@@ -1032,16 +1032,16 @@ jQuery(document).ready(function($) {
             // when window width is >= 480px
             480: {
                 slidesPerView: 1.5,
-                spaceBetween: 20,
+                spaceBetween: 30,
             },
             // when window width is >= 640px
             640: {
                 slidesPerView: 2,
-                spaceBetween: 20,
+                spaceBetween: 40,
             },
             991: {
                 slidesPerView: 4,
-                spaceBetween: 20,
+                spaceBetween: 50,
             }
         }
     });
