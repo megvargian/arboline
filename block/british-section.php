ad4a9caddccc2d69df581199c2a1c0656f4e0a90
Megvargian123 <mikekouyoumdjian@hotmail.com>
2025-08-23 01:35:39 +0300

    fix

--- block/british-section.php
+++ block/british-section.php
@@ -7,65 +7,39 @@ $text = $fields['british_text'];
 $link_url = $fields['british_link_url'];
 $link_text = $fields['british_link_text'];
 $lid_image = $fields['british_lid_image'];
-$dir_iamges = get_template_directory_uri() . '/inc/assets/images/';
 ?>
 <div class="wp-block-cover alignfull is-light mt-0" style="min-height:60vh">
-    <span aria-hidden="true"
-        class="wp-block-cover__background has-white-background-color has-background-dim-90 has-background-dim">
-    </span>
-    <img decoding="async" width="2560" height="551" class="wp-block-cover__image-background wp-image-2244" alt=""
-        src="<?php echo $dir_iamges; ?>wood-grain@2x-100-min-scaled.jpg" style="object-position:10% 54%"
-        data-object-fit="cover" data-object-position="10% 54%"
-        srcset="<?php echo $dir_iamges; ?>wood-grain@2x-100-min-scaled.jpg 2560w, <?php echo $dir_iamges; ?>wood-grain@2x-100-min-scaled-600x129.jpg 600w, <?php echo $dir_iamges; ?>wood-grain@2x-100-min-300x65.jpg 300w, <?php echo $dir_iamges; ?>wood-grain@2x-100-min-1024x220.jpg 1024w, <?php echo $dir_iamges; ?>wood-grain@2x-100-min-768x165.jpg 768w, <?php echo $dir_iamges; ?>wood-grain@2x-100-min-1536x330.jpg 1536w, <?php echo $dir_iamges; ?>wood-grain@2x-100-min-2048x440.jpg 2048w, <?php echo $dir_iamges; ?>wood-grain@2x-100-min-1320x284.jpg 1320w"
-        sizes="(max-width: 2560px) 100vw, 2560px">
+    <span aria-hidden="true" class="wp-block-cover__background has-white-background-color has-background-dim-90 has-background-dim"></span>
     <div class="wp-block-cover__inner-container">
-        <div class="wp-block-cover alignfull is-light contained-bg mb-0 pb-0"><span aria-hidden="true"
-                class="wp-block-cover__background has-secondary-background-color has-background-dim-0 has-background-dim"></span><img
-                decoding="async" loading="lazy" width="2560" height="1449"
-                class="wp-block-cover__image-background wp-image-2303" alt="" src="<?php echo $dir_iamges; ?>1896-1.png"
-                style="object-position:50% 48%" data-object-fit="cover" data-object-position="50% 48%"
-                srcset="<?php echo $dir_iamges; ?>1896-1.png 2560w, <?php echo $dir_iamges; ?>1896-1-600x340.png 600w, <?php echo $dir_iamges; ?>1896-1-300x170.png 300w, <?php echo $dir_iamges; ?>1896-1-1024x580.png 1024w, <?php echo $dir_iamges; ?>1896-1-768x435.png 768w, <?php echo $dir_iamges; ?>1896-1-1536x869.png 1536w, <?php echo $dir_iamges; ?>1896-1-2048x1159.png 2048w, <?php echo $dir_iamges; ?>1896-1-1320x747.png 1320w"
-                sizes="(max-width: 2560px) 100vw, 2560px">
+        <div class="wp-block-cover alignfull is-light contained-bg mb-0 pb-0">
             <div class="wp-block-cover__inner-container">
                 <div class="is-layout-flex wp-container-4 wp-block-columns are-vertically-aligned-center mb-0">
                     <div class="is-layout-flow wp-block-column is-vertically-aligned-center">
-                        <figure
-                            class="mx-auto justify-content-center d-flex align-items-center size-large is-resized mb-1">
-                            <img decoding="async" loading="lazy" src="<?php echo $dir_iamges; ?>uk-flag-1024x609.png"
-                                alt="" class="wp-image-2534" width="76" height="45"
-                                srcset="<?php echo $dir_iamges; ?>uk-flag-1024x609.png 1024w, <?php echo $dir_iamges; ?>uk-flag-600x357.png 600w, <?php echo $dir_iamges; ?>uk-flag-300x178.png 300w, <?php echo $dir_iamges; ?>uk-flag-768x457.png 768w, <?php echo $dir_iamges; ?>uk-flag.png 1116w"
-                                sizes="(max-width: 76px) 100vw, 76px">
+                        <figure class="mx-auto justify-content-center d-flex align-items-center size-large is-resized mb-1">
+                            <?php if ($flag_image): ?>
+                                <img src="<?php echo esc_url($flag_image); ?>" alt="British Flag" width="76" height="45">
+                            <?php endif; ?>
                         </figure>
-
-
-
                         <div style="height:20px" aria-hidden="true" class="wp-block-spacer d-lg-block d-none"></div>
-
-
-
-                        <h2 class="has-text-align-center underline h1"><span
-                                style="text-decoration: underline;">FERVENTLY BRITISH</span></h2>
-
-
-
-                        <p class="has-text-align-center has-medium-font-size">FIDDES has a long and proud history of
-                            caring for wood that began during the early part of last century and continues to this
-                            day.&nbsp;</p>
-
-
-
+                        <h2 class="has-text-align-center underline h1"><span style="text-decoration: underline;">
+                            <?php echo esc_html($title); ?>
+                        </span></h2>
+                        <p class="has-text-align-center has-medium-font-size"><?php echo esc_html($text); ?></p>
                         <div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
-
-
-
-                        <p class="has-text-align-center underline"><a href="" data-type="page" data-id="1509"><span
-                                    style="text-decoration: underline;">DISCOVER MORE</span></a>
+                        <p class="has-text-align-center underline">
+                            <?php if ($link_url && $link_text): ?>
+                                <a href="<?php echo esc_url($link_url); ?>"><span style="text-decoration: underline;">
+                                    <?php echo esc_html($link_text); ?>
+                                </span></a>
+                            <?php endif; ?>
                         </p>
                     </div>
                     <div class="is-layout-flow wp-block-column is-vertically-aligned-center mb-0">
-                        <figure class="wp-block-image size-full"><img decoding="async" loading="lazy" width="850"
-                                height="844" src="<?php echo $dir_iamges; ?>FIDDES-LID.png" alt="" class="wp-image-3288"
-                                sizes="(max-width: 850px) 100vw, 850px"></figure>
+                        <figure class="wp-block-image size-full">
+                            <?php if ($lid_image): ?>
+                                <img src="<?php echo esc_url($lid_image); ?>" alt="Lid Image" width="850" height="844">
+                            <?php endif; ?>
+                        </figure>
                     </div>
                 </div>
             </div>
