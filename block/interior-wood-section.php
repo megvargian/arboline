56899552c4e701b703347cdb3147313884713496
Megvargian123 <mikekouyoumdjian@hotmail.com>
2025-08-23 12:39:29 +0300

    fix

--- block/interior-wood-section.php
+++ block/interior-wood-section.php
@@ -7,25 +7,71 @@ $title = $fields['interior_wood_title'];
 $text = $fields['interior_wood_text'];
 $products = $fields['interior_wood_products']; // Repeater field
 ?>
-<div class="wp-block-cover alignfull is-light my-0" style="min-height:60vh">
-    <span aria-hidden="true" class="wp-block-cover__background has-white-background-color has-background-dim-90 has-background-dim"></span>
+<!-- <div class="wp-block-cover alignfull is-light my-0" style="min-height:60vh">
+    <span aria-hidden="true"
+        class="wp-block-cover__background has-white-background-color has-background-dim-90 has-background-dim"></span>
     <img src="<?php echo esc_url($background_image); ?>" alt="Background" width="2560" height="551">
     <div class="wp-block-cover__inner-container">
         <div class="is-layout-flow wp-block-group alignfull mb-0 has-background" style="background-color:#ffffff00">
-            <div class="is-layout-flex wp-container-19 wp-block-columns mb-0 pt-0 pt-lg-5 pb-0 gap-2 flex-column-reverse flex-lg-row">
+            <div
+                class="is-layout-flex wp-container-19 wp-block-columns mb-0 pt-0 pt-lg-5 pb-0 gap-2 flex-column-reverse flex-lg-row">
                 <div class="is-layout-flow wp-block-column is-vertically-aligned-top">
-                    <h3 class="has-text-align-center underline h1"><span style="text-decoration: underline;"><?php echo esc_html($title); ?></span></h3>
-                    <p class="has-text-align-center mb-lg-5 mb-4 has-medium-font-size"><?php echo esc_html($text); ?></p>
+                    <h3 class="has-text-align-center underline h1"><span
+                            style="text-decoration: underline;"><?php echo esc_html($title); ?></span></h3>
+                    <p class="has-text-align-center mb-lg-5 mb-4 has-medium-font-size"><?php echo esc_html($text); ?>
+                    </p>
                     <?php if ($products): ?>
-                        <?php foreach ($products as $product): ?>
-                            <p class="has-text-align-center mb-1 mt-2 has-medium-font-size">
-                                <a href="<?php echo esc_url($product['url']); ?>"><?php echo esc_html($product['name']); ?></a>
-                            </p>
-                        <?php endforeach; ?>
+                    <?php foreach ($products as $product): ?>
+                    <p class="has-text-align-center mb-1 mt-2 has-medium-font-size">
+                        <a href="<?php echo esc_url($product['url']); ?>"><?php echo esc_html($product['name']); ?></a>
+                    </p>
+                    <?php endforeach; ?>
                     <?php endif; ?>
                 </div>
                 <div class="is-layout-flow wp-block-column is-vertically-aligned-center">
-                    <figure><img src="<?php echo esc_url($finishes_image); ?>" alt="Finishes" width="896" height="794"></figure>
+                    <figure><img src="<?php echo esc_url($finishes_image); ?>" alt="Finishes" width="896" height="794">
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+
+<div class="wp-block-cover alignfull is-light my-0" style="min-height:60vh"><span aria-hidden="true"
+        class="wp-block-cover__background has-background-dim-10 has-background-dim"
+        style="background-color:#717171"></span><img decoding="async" loading="lazy" width="2000" height="430"
+        class="wp-block-cover__image-background wp-image-2499" alt="" src="<?php echo esc_url($background_image); ?>"
+        style="object-position:9% 35%" data-object-fit="cover" data-object-position="9% 35%"
+        sizes="(max-width: 2000px) 100vw, 2000px">
+    <div class="wp-block-cover__inner-container">
+        <div class="is-layout-flow wp-block-group alignfull mb-0">
+            <div
+                class="is-layout-flex wp-container-15 wp-block-columns are-vertically-aligned-top mb-0 pt-0 pt-lg-5 pb-0 gap-2">
+                <div class="is-layout-flow wp-block-column is-vertically-aligned-top">
+                    <figure class="wp-block-image size-full mb-0"><img decoding="async" loading="lazy" width="896"
+                            height="794" src="<?php echo esc_url($finishes_image); ?>" alt="" class="wp-image-3252"
+                            sizes="(max-width: 896px) 100vw, 896px"></figure>
+                </div>
+
+
+
+                <div class="is-layout-flow wp-block-column is-vertically-aligned-top">
+                    <div style="height:10px" aria-hidden="true" class="wp-block-spacer d-none d-lg-block"></div>
+
+
+
+                    <h3 class="has-text-align-center underline h1"><span style="text-decoration: underline;"><a
+                                href="#"><?php echo esc_html($title); ?></a></span></h3>
+
+
+
+                    <?php if ($products): ?>
+                    <?php foreach ($products as $product): ?>
+                    <p class="has-text-align-center mb-1 mt-2 has-medium-font-size">
+                        <a href="<?php echo esc_url($product['url']); ?>"><?php echo esc_html($product['name']); ?></a>
+                    </p>
+                    <?php endforeach; ?>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
