c479bc7ab58419db205a81766b375498808c0d2f
Megvargian123 <mikekouyoumdjian@hotmail.com>
2025-09-01 13:09:35 +0300

    add guide block

--- block/guide_block.php
+++ block/guide_block.php
@@ -8,13 +8,13 @@ $fields = get_fields();
         <?php foreach ($fields['guides'] as $key => $guide) { ?>
             <div class="col-12 col-lg-3 video-guides--single">
                 <div class="flex-column d-flex">
-                    <a href="<?php echo $guide['url']; ?>" data-gallery="videoguides"
+                    <a href="<?php echo $guide['youtubeurl']; ?>" data-gallery="videoguides"
                         class="glightbox-video overflow-hidden ratio ratio-16x9" target="_blank" rel="noopener">
                         <img decoding="async" src="<?php echo $guide['image']; ?>"
                             class="w-100">
                     </a>
                     <div class="w-100 bg-gray-100 p-4 d-flex flex-column justify-content-between">
-                        <a href="<?php echo $guide['url']; ?>" data-gallery="videoguides"
+                        <a href="<?php echo $guide['youtubeurl']; ?>" data-gallery="videoguides"
                             class="glightbox-video d-flex justify-content-between" target="_blank" rel="noopener">
                             <strong>WATCH GUIDE</strong>
                             <i class="bi bi-play-circle-fill fs-5"></i>
