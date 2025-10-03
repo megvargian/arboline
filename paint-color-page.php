3b6f28d679736e1dfcfca38bda7bc9ac462c92a8
Megvargian123 <mikekouyoumdjian@hotmail.com>
2025-10-03 09:27:52 +0300

    add paint colors page

---
+++ paint-color-page.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: Homepage
+ */
+get_header();
+?>
+<div class="hero">
+    <h1>Welcome to Our Paint Color Page</h1>
+    <p>Explore a wide range of colors for your next project.</p>
+</div>
+<?php
+get_footer();
