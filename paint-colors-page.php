6c897808ea36bd5d4ab26118b86d652b57d62ee6
Megvargian123 <mikekouyoumdjian@hotmail.com>
2025-10-03 09:31:51 +0300

    add paint colors page

--- paint-colors-page.php
+++ paint-colors-page.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Template Name: Homepage
+ * Template Name: Paint Colors Page
  */
 get_header();
 ?>
 <div class="hero">
-    <h1>Welcome to Our Paint Color Page</h1>
+    <h1>Welcome to Our Paint Colors Page</h1>
     <p>Explore a wide range of colors for your next project.</p>
 </div>
 <?php
