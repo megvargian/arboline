8c6f41230813a20cd9e817cbbdb418c639201a63
Megvargian123 <mikekouyoumdjian@hotmail.com>
2025-10-04 21:38:10 +0300

    fix filter

--- functions.php
+++ functions.php
@@ -2044,7 +2044,7 @@ function filter_products_by_tint() {
     }
 
     // Generate HTML for matched products (4 per row like related products)
-    ob_start();
+    // ob_start();
 
     $count = 0;
     foreach ($matched_products as $product_id) {
@@ -2069,7 +2069,7 @@ function filter_products_by_tint() {
         $count++;
     }
 
-    $html = ob_get_clean();
+    // $html = ob_get_clean();
 
     wp_send_json_success(array(
         'html' => $html,
